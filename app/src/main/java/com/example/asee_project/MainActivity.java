28782da81f411c9eb2ffebd62d16df1836e70d86
villarias <72759374+villarias@users.noreply.github.com>
2020-11-04 14:09:00 +0100

    creacion

---
+++ app/src/main/java/com/example/asee_project/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.asee_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.i("Commit-prueba","Commit el abel");
+    }
+}
